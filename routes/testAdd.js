ed871e1689f613fade643a06014d22230e19aac0
bkh7c7 <bkh7c7@gmail.com>
2020-12-15 21:44:04 +0900

    update

---
+++ routes/testAdd.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+module.exports.community = function(req,res){
+    res.render('community');
+}
+module.exports.notice = function(req,res){
+    res.render('notice');
+}
+module.exports.release = function(req,res){
+    res.render('release');
+}
+module.exports.roomate = function(req,res){
+    res.render('roomate');
+}
+module.exports.roomdetail = function(req,res){
+    res.render('roomdetail');
+}
+module.exports.signup = function(req,res){
+    res.render('signup');
+}
+module.exports.signin = function(req,res){
+    res.render('signin');
+}
